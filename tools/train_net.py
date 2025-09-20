4f7641ef09848b1c6e98efc61a7146539b89e9a5
2026wacv692 <bossbabyargentina@gmail.com>
2025-09-21 04:13:56 +0900

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -9,10 +9,10 @@ from detectron2.engine import default_argument_parser, default_setup, launch
 
 from ms_depro import add_cfg
 from ms_depro.data.datasets import builtin
-from ms_depro.engine.train_loop import MSLTrainer
-from ms_depro.modeling.meta_arch.rcnn import MSLCLIPRCNN
+from ms_depro.engine.train_loop import MSTrainer
+from ms_depro.modeling.meta_arch.rcnn import MSCLIPRCNN
 from ms_depro.modeling.backbone import build_clip_resnet_backbone
-from ms_depro.modeling.proposal_generator.rpn import MSLRPN
+from ms_depro.modeling.proposal_generator.rpn import MSRPN
 from ms_depro.modeling.roi_heads.clip_roi_heads import CLIPRes5ROIHeads
 from ms_depro.modeling.meta_arch.ts_ensemble import EnsembleTSModel
 
@@ -34,24 +34,24 @@ def main(args):
     cfg = setup(args)
     
     if args.eval_only:
-        msl = cfg.TEST.MSL_MODE
-        model_student = MSLTrainer.build_model(cfg)
-        model_teacher = MSLTrainer.build_model(cfg)
+        ms = cfg.TEST.MS_MODE
+        model_student = MSTrainer.build_model(cfg)
+        model_teacher = MSTrainer.build_model(cfg)
         ts_model = EnsembleTSModel(model_teacher, model_student)
 
         DetectionCheckpointer(ts_model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
             cfg.MODEL.WEIGHTS, resume=args.resume
         )
-        assert msl in ["MSDA", "MSDG"]
+        assert ms in ["MSDA", "MSDG"]
         
-        res = MSLTrainer.test(cfg, ts_model.modelTeacher if msl == "MSDA" else ts_model.modelStudent)    
+        res = MSTrainer.test(cfg, ts_model.modelTeacher if ms == "MSDA" else ts_model.modelStudent)    
         return res
     """
     If you'd like to do anything fancier than the standard training logic,
     consider writing your own training loop (see plain_train_net.py) or
     subclassing the trainer.
     """
-    trainer = MSLTrainer(cfg)
+    trainer = MSTrainer(cfg)
     trainer.resume_or_load(resume=args.resume)
     
     return trainer.train()
